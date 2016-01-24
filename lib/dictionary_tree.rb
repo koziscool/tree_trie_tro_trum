87ddb301b97572320386aaeefcd2e72d3413a787
koziscool <kozjob@yahoo.com>
2016-01-23 18:01:32 -0600

    some refactoring, tests still pass

--- lib/dictionary_tree.rb
+++ lib/dictionary_tree.rb
@@ -17,7 +17,7 @@ class DictionaryTree
       word = word_def[0].split("")
       parent = @root
       word.each_with_index do |c, i|
-        child = get_child_node( parent, c )
+        child = find_or_create_child_node( parent, c )
         child.words_on_branch += 1
         child.definition = word_def[1] if i == word.length - 1
         parent = child
@@ -26,17 +26,26 @@ class DictionaryTree
     end
   end
 
-  def get_child_node( node, letter )
-    node.children.each do |child_node|
+  def find_or_create_child_node( parent, letter )
+    existing_child = get_child_node(parent, letter )
+    existing_child ? existing_child : create_child_node( parent, letter )
+  end
+
+  def get_child_node( parent, letter )
+    parent.children.each do |child_node|
       return child_node if child_node.letter == letter
     end
+    nil
+  end
+
+  def create_child_node( parent, letter )
     new_child = LetterNode.new
     new_child.letter = letter
-    new_child.parent = node
-    new_child.depth = node.depth + 1
+    new_child.parent = parent
+    new_child.depth = parent.depth + 1
     new_child.children = []
-    new_child.words_on_branch = 0    
-    node.children << new_child
+    new_child.words_on_branch = 0
+    parent.children << new_child
     @num_letters += 1
     new_child
   end
@@ -45,24 +54,28 @@ class DictionaryTree
     cursor = @root
     word_arr = word.split("")
     word_arr.each do |c|
-      found_letter = false
-      cursor.children.each do |child_node|
-        if child_node.letter == c
-          cursor = child_node
-          found_letter = true
-          next
-        end
-      end
-      return nil unless found_letter
+      existing_child = get_child_node( cursor, c )
+      existing_child ? ( cursor = existing_child ) : (return nil)
     end
     cursor.definition
+  end
+
+  def end_node_for( word )
+    parent = @root
+    word_arr = word.split("")
+    word_arr.each do |c|
+      existing_child = get_child_node( parent, c )
+      return nil unless existing_child
+      parent = existing_child
+    end 
+    parent   
   end
 
   def insert_word( word, definition )
     parent = @root
     word_arr = word.split("")
     word_arr.each do |c|
-      child = get_child_node( parent, c )
+      child = find_or_create_child_node( parent, c )
       child.words_on_branch += 1
       child.definition = definition if child.depth == word.length
       parent = child
@@ -70,27 +83,27 @@ class DictionaryTree
     @num_words += 1
   end
 
+
   def remove_word( word )
+    definition_node = end_node_for( word )
+    return unless definition_node
+
+    num_letters_to_be_removed = 0 
     parent = @root
     word_arr = word.split("")
-    num_letters_to_be_removed = 0 
     word_arr.each do |c|
-      child = nil
-      parent.children.each do |node|
-        child = node if node.letter == c
-      end
-      return if child == nil
-      child.words_on_branch -= 1
-      if child.words_on_branch == 0
-        parent.children.delete( child )
+      existing_child = get_child_node( parent, c )
+      existing_child.words_on_branch -= 1
+      if existing_child.words_on_branch == 0
+        parent.children.delete( existing_child )
         num_letters_to_be_removed += 1
-        parent = child
-        child = nil
+        parent = existing_child
+        existing_child = nil
       else
-        parent = child
+        parent = existing_child
       end
     end
-    parent.definition = nil
+    definition_node.definition = nil
     @num_letters -= num_letters_to_be_removed
     @num_words -= 1
   end
@@ -105,6 +118,5 @@ class DictionaryTree
     dive( @root )
     @max_depth
   end
-
 
 end
--- lib/letter_node.rb
+++ lib/letter_node.rb
@@ -2,3 +2,4 @@
 
 LetterNode = Struct.new( :letter, :definition, 
     :children, :parent, :depth, :words_on_branch )
+
